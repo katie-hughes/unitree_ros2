a1c7b52265a409a11ff84710a10694545e64f598
Nick Morales <ngmorales97@gmail.com>
2023-01-24 18:45:56 -0600

    Rewrote UDP high node as a class

--- unitree_ros2_to_real/src/udp_high.cpp
+++ unitree_ros2_to_real/src/udp_high.cpp
@@ -10,7 +10,7 @@ using UNITREE_LEGGED_SDK::UDP;
 using UNITREE_LEGGED_SDK::HighCmd;
 using UNITREE_LEGGED_SDK::HighState;
 
-class UDPBridgeHigh
+class UDPHighBridge
 {
 public:
     UDP udp;
@@ -19,55 +19,64 @@ public:
     HighState state = {0};
 
 public:
-    UDPBridgeHigh()
+    UDPHighBridge()
         : udp(8090, "192.168.123.161", 8082, sizeof(HighCmd), sizeof(HighState))
     {
         udp.InitCmdData(cmd);
     }
 };
 
-UDPBridgeHigh bridge;
-
-rclcpp::Subscription<ros2_unitree_legged_msgs::msg::HighCmd>::SharedPtr sub_high;
-rclcpp::Publisher<ros2_unitree_legged_msgs::msg::HighState>::SharedPtr pub_high;
-
-long high_count = 0;
-
-void highCmdCallback(const ros2_unitree_legged_msgs::msg::HighCmd::SharedPtr msg)
+class UDPHighNode : public rclcpp::Node
 {
-    printf("highCmdCallback is running !\t%ld\n", ::high_count);
+public:
+    UDPHighNode()
+    : Node("udp_high")
+    {
+        timer_ = create_wall_timer(
+            2ms, //TODO - make this configurable
+            std::bind(&UDPHighNode::timer_callback, this)
+        );
 
-    bridge.cmd = rosMsg2Cmd(msg);
+        pub_state_ = this->create_publisher<ros2_unitree_legged_msgs::msg::HighState>("high_state", 10);
+        sub_cmd_ = this->create_subscription<ros2_unitree_legged_msgs::msg::HighCmd>(
+            "high_cmd",
+            10,
+            std::bind(&UDPHighNode::cmd_callback, this, std::placeholders::_1)
+        );
+    }
+private:
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<ros2_unitree_legged_msgs::msg::HighState>::SharedPtr pub_state_;
+    rclcpp::Subscription<ros2_unitree_legged_msgs::msg::HighCmd>::SharedPtr sub_cmd_;
+    UDPHighBridge bridge_;
+    ros2_unitree_legged_msgs::msg::HighState state_ros_;
 
-    bridge.udp.SetSend(bridge.cmd);
-    bridge.udp.Send();
+    void cmd_callback(const ros2_unitree_legged_msgs::msg::HighCmd::SharedPtr msg)
+    {
+        //Convert ROS message to UDP command
+        bridge_.cmd = rosMsg2Cmd(msg);
 
-    ros2_unitree_legged_msgs::msg::HighState state_ros;
+        //Send UDP command
+        bridge_.udp.SetSend(bridge_.cmd);
+        bridge_.udp.Send();
+    }
 
-    bridge.udp.Recv();
-    bridge.udp.GetRecv(bridge.state);
+    void timer_callback()
+    {
+        //Get state over UDP
+        bridge_.udp.Recv();
+        bridge_.udp.GetRecv(bridge_.state);
 
-    state_ros = state2rosMsg(bridge.state);
-
-    pub_high->publish(state_ros);
-
-    printf("highCmdCallback ending !\t%ld\n\n", ::high_count++);
-}
+        //Publish state as ROS message
+        state_ros_ = state2rosMsg(bridge_.state);
+        pub_state_->publish(state_ros_);
+    }
+};
 
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-
-    auto node = rclcpp::Node::make_shared("udp_high");
-
-    printf("high level running!\n");
-
-    pub_high = node->create_publisher<ros2_unitree_legged_msgs::msg::HighState>("high_state", 1);
-    sub_high = node->create_subscription<ros2_unitree_legged_msgs::msg::HighCmd>("high_cmd", 1, highCmdCallback);
-
-    rclcpp::spin(node);
-
+    rclcpp::spin(std::make_shared<UDPHighNode>());
     rclcpp::shutdown();
-
     return 0;
 }
