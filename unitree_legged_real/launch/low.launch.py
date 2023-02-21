6e2947029f6ca6faae01245bacbbfe80f879dd33
Katie Hughes <katiereeshughes@gmail.com>
2023-02-21 16:01:39 -0600

    condense low_visualize.launch into low.launch

--- unitree_legged_real/launch/low.launch.py
+++ unitree_legged_real/launch/low.launch.py
@@ -16,11 +16,24 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 def generate_launch_description():
      return LaunchDescription([
 
+          DeclareLaunchArgument(
+            name='use_rviz',
+            default_value='true',
+            choices=['true','false'],
+            description='Open RVIZ for Go1 visualization'),
+
           Node(package='unitree_legged_real',
                executable='udp_low',
                output='screen'),
-
-          Node(package='unitree_legged_real',
-               executable='custom_gait',
-               output='screen')
+               
+          IncludeLaunchDescription(
+               PythonLaunchDescriptionSource(
+                    PathJoinSubstitution([FindPackageShare('go1_description'),
+                                                           'launch',
+                                                           'visualize.launch.py'])),
+                    launch_arguments=[
+                         ('use_jsp', 'none'),
+                         ('publish_static_world_tf', 'true'),
+                         ('fixed_frame', 'world')
+                    ])
     ])
