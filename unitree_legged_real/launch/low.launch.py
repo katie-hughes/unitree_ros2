ec928661243a56aae0fee0174ea1162f1c0a0c9c
Katie Hughes <katiereeshughes@gmail.com>
2023-02-22 13:54:47 -0600

    change structure of low launch file to take enable_state and enable_cmd params so that the udp node does not get launched twice

--- unitree_legged_real/launch/low.launch.py
+++ unitree_legged_real/launch/low.launch.py
@@ -16,26 +16,44 @@ def generate_launch_description():
      return LaunchDescription([
 
           DeclareLaunchArgument(
-            name='js_source',
-            default_value='state',
-            choices=['cmd','state'],
-            description='Determines whether sent commands (cmd) or current state (src) '+
-                        'are published to the /joint_state topic.'),
+            name='enable_cmd',
+            default_value='true',
+            choices=['true','false'],
+            description='Determines whether to start a robot model '+
+                        'corresponding to the /low_cmd topic (i.e., desired positions)'),
+
+          DeclareLaunchArgument(
+            name='enable_state',
+            default_value='true',
+            choices=['true','false'],
+            description='Determines whether to start a robot model '+
+                        'corresponding to the /low_state topic (i.e., real positions)'),
 
           DeclareLaunchArgument(name='use_rviz', default_value='true',
                                 choices=['true', 'false'],
                                 description='Choose if rviz is launched'),
 
+          # Always run the udp!
           Node(package='unitree_legged_real',
                executable='udp_low',
                output='screen'),
 
+          # Conditionally run the jsp's based on launch arguments
           Node(package='unitree_legged_real',
                executable='jsp_low',
-               parameters=[{"js_source": LaunchConfiguration('js_source')}],
-               namespace=LaunchConfiguration('js_source'),
+               parameters=[{"js_source": "cmd"}],
+               namespace="cmd",
+               condition=IfCondition(LaunchConfiguration('enable_cmd')),
                output='screen'),
-               
+
+          Node(package='unitree_legged_real',
+               executable='jsp_low',
+               parameters=[{"js_source": "state"}],
+               namespace="state",
+               condition=IfCondition(LaunchConfiguration('enable_state')),
+               output='screen'),
+
+          # Load robot models depending on state
           IncludeLaunchDescription(
                PythonLaunchDescriptionSource(
                     PathJoinSubstitution([FindPackageShare('go1_description'),
@@ -43,8 +61,36 @@ def generate_launch_description():
                                                            'load_go1.launch.py'])),
                     launch_arguments=[
                          ('use_jsp', 'none'),
-                         ('fixed_frame', 'world'),
-                         ('namespace', LaunchConfiguration('js_source')),
+                         ('namespace', 'cmd'),
                          ('use_rviz', LaunchConfiguration('use_rviz'))
-                    ])
+                    ],
+               condition=IfCondition(LaunchConfiguration('enable_cmd'))
+          ),
+
+          IncludeLaunchDescription(
+               PythonLaunchDescriptionSource(
+                    PathJoinSubstitution([FindPackageShare('go1_description'),
+                                                           'launch',
+                                                           'load_go1.launch.py'])),
+                    launch_arguments=[
+                         ('use_jsp', 'none'),
+                         ('namespace', 'state'),
+                         ('use_rviz', LaunchConfiguration('use_rviz'))
+                    ],
+               condition=IfCondition(LaunchConfiguration('enable_state'))
+          ),
+
+          # Generic model that gets loaded if both enable_cmd and enable_state are false
+          # With no namespaces
+          # IncludeLaunchDescription(
+          #      PythonLaunchDescriptionSource(
+          #           PathJoinSubstitution([FindPackageShare('go1_description'),
+          #                                                  'launch',
+          #                                                  'load_go1.launch.py'])),
+          #           launch_arguments=[
+          #                ('use_jsp', 'none'),
+          #                ('use_rviz', LaunchConfiguration('use_rviz'))
+          #           ],
+          #      condition=IfCondition(LaunchConfiguration('enable_cmd'))
+          # ),
     ])
